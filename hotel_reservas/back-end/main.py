cd017bf1d097937319676345f55ae082cc91b5e1
Luiz <162625353+LuixGra@users.noreply.github.com>
2025-08-27 13:41:05 -0300

    Update main.py

--- hotel_reservas/back-end/main.py
+++ hotel_reservas/back-end/main.py
@@ -25,7 +25,7 @@ def get_connection():
     conn = psycopg2.connect(
         dbname="hotel_reservations",
         user="postgres",
-        password="240902",
+        password="SENHA",
         host="localhost",
         port="5432",
         cursor_factory=RealDictCursor
